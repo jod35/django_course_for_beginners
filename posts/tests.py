99a13ab56612e3ccb0b70994c65fe617c0082e0b
jod35 <jodestrevin@gmail.com>
2022-03-17 21:16:14 +0300

    unit testing

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
+
+
+class PostTestCase(TestCase):
+
+    def setUp(self):
+        self.post=Post.objects.create(
+            title="Test title",
+            author="Test author",
+            content="Test content"
+        )
+    
+
+    def test_home_page(self):
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'index.html')
+        self.assertContains(response,"List of posts")
+
+    def test_post_creation(self):
+        
+        post_to_test=Post.objects.get(id=1)
+
+        post_title=f"{post_to_test.title}"
+
+        self.assertEqual(self.post.title,post_title)
+        self.assertEqual(self.post.title,"Test title")
+        self.assertEqual(self.post.author,"Test author")
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+        self.user=get_user_model().objects.create(
+            username="testuser",
+            email="testuser@company.com",
+            password="test123"
+        )
+
+    
+    def test_usercreation(self):
+        user=get_user_model().objects.get(id=1)
+
+        self.assertEqual(user.is_staff,False)
+        self.assertEqual(self.user.username,user.username)
